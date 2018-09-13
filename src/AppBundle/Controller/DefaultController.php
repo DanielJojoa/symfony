41b96c343b609e2db7919c024dd7398874e96a2b
Carlos Guadir Aza <carlos@udenar.edu.co>
2018-09-13 14:28:13 -0500

    funciona sin token

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Services\Helpers;
+use AppBundle\Services\JwtAuth;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class DefaultController extends Controller
+{
+   
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+
+    }
+    public function loginAction(Request $request)
+    {
+        $helpers = $this->get(Helpers::Class);
+        //reucibir  json por Post
+        $json = $request->get('json',null);
+        //array para devolver por defecto
+        $data = array(
+            'status' =>'error1',
+            'data' =>'Send json via Post ¡¡'
+        );
+        if($json != null)
+        {
+            $params = json_decode($json);
+            $email =(isset($params->email)) ? $params->email:null;
+            $password =(isset($params->password)) ? $params->password:null;
+            
+            $emailConstranint = new Assert\Email();
+            $emailConstranint->message = "This email is not valid";
+            $validate_email = $this->get("validator")->validate($email,$emailConstranint);
+            var_dump($password);
+            if( $email != null && count($validate_email) == 0 && $password != null)
+            { 
+                $jwt_auth = $this->get(JwtAuth::Class);
+                $signup = $jwt_auth->signup($email,$password); 
+                $data = array(
+                      'status' =>'success',
+                       'data' =>'ok',
+                       'signup' => $signup
+                
+                            );
+            }
+            else
+            {
+                $data = array(
+                      'status' =>'error2',
+                       'data' =>'Password or email is wrong'
+                
+                            );
+            }
+        }else
+        {
+
+        }
+        return $helpers->json($data);
+    }
+    public function pruebasAction(){
+        $em = $this->GetDoctrine()->getManager();
+        $userRepo = $em->getRepository('BackendBundle:User');
+        $users = $userRepo->findAll();
+        //var_dump($users);
+        $helper = $this->get(Helpers::Class);
+        return $helper->json($users);   
+        /*return $this->json(array(
+        'status' => 'success',
+            'users' => $users[0]->getName()
+        ));
+        die();*/
+    }
+}
---
+++ src/AppBundle/Services/JwtAuth.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Services;
+
+use Firebase\JWT\JWT;
+class JwtAuth
+{ 
+    public $manager;
+    public $key;
+    public function __construct($manager){
+        $this->manager = $manager;
+        $this->key = "holaquetalsoylaclavesecreta12345";
+    } 
+    public function signup($email,$password, $getHash = null)
+    {   $user = $this->manager->getRepository('BackendBundle:User')->findOneBy(array(
+        "email" => $email,
+        "password" =>$password
+    ));
+    $signup  = false;
+        if(is_object($user))
+        {
+            $signup = true;
+        }
+        if ($signup == true)
+        {   
+            /*$token = array(
+                "sub" => $user->getId(),
+                "email" => $user->getEmail(),
+                "name" => $user->getName(),
+                "surname" => $user->getSurname(),
+                "iat" => time(),
+                "exp" => time()+(7*24*60*60)
+
+            );
+            $jwt = JWT::encode($token,$this->key,'HS256');
+            $deconded = JWT::decode($jwt,$this->key,array('HS256'));
+            if($getHash == null || $getHash == false){
+                $data =$jwt;
+            }
+            else{
+                $data = $deconded;
+            }
+            $data = $jwt;*/
+            return $password." ".$email;
+        }else{
+            $data = array (
+                "status"=> "error",
+                "user" => "Login Failed"
+        
+            );
+        }
+        return $data;
+    }
+}
